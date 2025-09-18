e330c965b0517abf65b2caf753b847fb31f72750
prem sagar <akulapremsagar@gmail.com>
2025-09-18 23:47:13 +0530

    Initial commit

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
 
 var app = express();
-
+require('./app_server/models/db');
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname,'app_server','views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
@@ -35,7 +35,11 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    title: 'Error',
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  });
 });
 
 module.exports = app;
--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,3 +1,3 @@
 exports.home = function (req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Perfect_Fitt' });
 };
